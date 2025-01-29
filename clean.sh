2e047adb8ef035fa19803833f909137527565e9f
Ekpenyong-Esu <eeg21003@student.mdu.se>
2025-01-29 18:33:30 +0100

    assignment-5-complete

--- clean.sh
+++ clean.sh
@@ -1,8 +1,19 @@
-
 #!/bin/sh
+
+
+
 
 # Navigate to the Buildroot directory
 cd buildroot || { echo "Failed to navigate to buildroot directory"; exit 1; }
 
+
+rm -rf output/
+rm -rf ~/.buildroot-ccache
+rm -rf dl/
+
 # Run make distclean to clean the Buildroot build directory
 make distclean || { echo "Failed to clean the Buildroot build directory"; exit 1; }
+
+
+
+
